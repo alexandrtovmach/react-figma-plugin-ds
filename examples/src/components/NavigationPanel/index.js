71853c98ad61bb2ffd0952b83fa4e1a58ce94f1a
alexandrtovmach <alexandrtovmach@gmail.com>
2019-12-14 22:42:51 +0200

    feat: Styled navigation

--- examples/src/components/NavigationPanel/index.js
+++ examples/src/components/NavigationPanel/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { Tip } from "../../../../dist";
 import "./NavigationPanel.scss";
 
 export default class NavigationPanel extends React.Component {
@@ -12,12 +13,24 @@ export default class NavigationPanel extends React.Component {
   render() {
     return (
       <aside className="navigation-panel">
-        <a href="#buttons">Button</a>
-        <a href="#checkbox">Checkbox</a>
-        <a href="#disclosure">Disclosure</a>
-        <a href="#input">Input</a>
-        <a href="#text">Text</a>
-        <a href="#icon">Icon</a>
+        <a href="#buttons">
+          <Tip iconName="frame">Button</Tip>
+        </a>
+        <a href="#checkbox">
+          <Tip iconName="frame">Checkbox</Tip>
+        </a>
+        <a href="#disclosure">
+          <Tip iconName="frame">Disclosure</Tip>
+        </a>
+        <a href="#input">
+          <Tip iconName="frame">Input</Tip>
+        </a>
+        <a href="#text">
+          <Tip iconName="frame">Text</Tip>
+        </a>
+        <a href="#icon">
+          <Tip iconName="frame">Icon</Tip>
+        </a>
       </aside>
     );
   }
