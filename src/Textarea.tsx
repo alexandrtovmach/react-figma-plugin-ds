d8b2a349f121cc03170450a92aafa203b0b87db0
Klemensas Dranseika <hello@klemensas.net>
2021-01-21 18:47:21 +0200

    feat: add ref and name support to textarea

--- src/Textarea.tsx
+++ src/Textarea.tsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 import { TextareaProps } from "../index";
 
-const Textarea: React.FunctionComponent<TextareaProps> = ({
-  className,
-  rows = 2,
-  defaultValue,
-  placeholder,
-  isDisabled,
-  onChange,
-}) => {
-  className = className || "";
-  return (
-    <textarea
-      rows={rows}
-      className={`textarea ${className}`}
-      placeholder={placeholder}
-      defaultValue={defaultValue}
-      disabled={isDisabled}
-      onChange={(event) => onChange && onChange(event.target.value, event)}
-    />
-  );
-};
+const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (
+    {
+      name,
+      className,
+      rows = 2,
+      defaultValue,
+      placeholder,
+      isDisabled,
+      onChange,
+    },
+    ref
+  ) => {
+    className = className || "";
+    return (
+      <textarea
+        name={name}
+        rows={rows}
+        className={`textarea ${className}`}
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+        disabled={isDisabled}
+        onChange={(event) => onChange && onChange(event.target.value, event)}
+        ref={ref}
+      />
+    );
+  }
+);
 
 export default Textarea;
