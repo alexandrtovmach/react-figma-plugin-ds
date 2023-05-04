97349c128e3f7f3aaf2adcb7119506e888cc50c3
shnyder <ajs.private@gmail.com>
2023-05-04 19:03:22 +0200

    input control forwards refs

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,76 +1,89 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 import { InputProps, InputWithIconProps } from "../index";
 import { Icon } from "./";
 
-const InputComponent: React.FunctionComponent<InputProps> = ({
-  className = "",
-  type,
-  defaultValue,
-  placeholder,
-  isDisabled,
-  onChange,
-  ...htmlInputProps
-}) => (
-  <input
-    {...htmlInputProps}
-    type={type}
-    className={className}
-    placeholder={placeholder}
-    defaultValue={defaultValue}
-    disabled={isDisabled}
-    onChange={(event) => onChange && onChange(event.target.value, event)}
-  />
+const InputComponent = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className = "",
+      type,
+      defaultValue,
+      placeholder,
+      isDisabled,
+      onChange,
+      ...htmlInputProps
+    },
+    ref
+  ) => (
+    <input
+      {...htmlInputProps}
+      ref={ref}
+      type={type}
+      className={className}
+      placeholder={placeholder}
+      defaultValue={defaultValue}
+      disabled={isDisabled}
+      onChange={(event) => onChange && onChange(event.target.value, event)}
+    />
+  )
 );
 
-const Input: React.FunctionComponent<InputWithIconProps> = ({
-  className,
-  type,
-  icon,
-  iconColor = "black3",
-  defaultValue,
-  placeholder,
-  isDisabled,
-  onChange,
-  children,
-  ...htmlInputProps
-}) => {
-  className = className || "";
-  type = type || "text";
-  const inputClass = "input__field";
+const Input = forwardRef<HTMLInputElement, InputWithIconProps>(
+  (
+    {
+      className,
+      type,
+      icon,
+      iconColor = "black3",
+      defaultValue,
+      placeholder,
+      isDisabled,
+      onChange,
+      children,
+      ...htmlInputProps
+    },
+    ref
+  ) => {
+    className = className || "";
+    type = type || "text";
+    const inputClass = "input__field";
 
-  if (icon) {
-    return (
-      <div className="input input--with-icon">
-        <Icon name={icon} color={iconColor} isDisabled={isDisabled} />
-        <InputComponent
-          {...htmlInputProps}
-          className={`${inputClass} ${className}`}
-          type={type}
-          defaultValue={defaultValue}
-          placeholder={placeholder}
-          isDisabled={isDisabled}
-          onChange={onChange}
-        />
-        {children}
-      </div>
-    );
-  } else {
-    return (
-      <div className="input">
-        <InputComponent
-          {...htmlInputProps}
-          className={`${inputClass} ${className}`}
-          type={type}
-          defaultValue={defaultValue}
-          placeholder={placeholder}
-          isDisabled={isDisabled}
-          onChange={onChange}
-        />
-        {children}
-      </div>
-    );
+    if (icon) {
+      return (
+        <div className="input input--with-icon">
+          <Icon name={icon} color={iconColor} isDisabled={isDisabled} />
+          <InputComponent
+            {...htmlInputProps}
+            ref={ref}
+            className={`${inputClass} ${className}`}
+            type={type}
+            defaultValue={defaultValue}
+            placeholder={placeholder}
+            isDisabled={isDisabled}
+            onChange={onChange}
+          />
+          {children}
+        </div>
+      );
+    } else {
+      return (
+        <div className="input">
+          <InputComponent
+            {...htmlInputProps}
+            ref={ref}
+            className={`${inputClass} ${className}`}
+            type={type}
+            defaultValue={defaultValue}
+            placeholder={placeholder}
+            isDisabled={isDisabled}
+            onChange={onChange}
+          />
+          {children}
+        </div>
+      );
+    }
   }
-};
+);
 
 export default Input;
